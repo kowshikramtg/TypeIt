de96ca6b0f337e9e7ff4a70653f4146ef7422438
kowshikramtg <kowshik1618@gmail.com>
2026-05-27 14:30:40 +0530

    updated curosr, and theme, and some smooth cursor animation

--- src/components/TypingBox.tsx
+++ src/components/TypingBox.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import generateWords from "../utils/generateWords";
 import themes from "../data/theme";
+import { motion } from "framer-motion";
 
 const TypingBox = () => {
   const [words, setWords] = useState("");
   const [input, setInput] = useState("");
-  const [timeLeft, setTimeLeft] = useState(30);
+  const [testTime, setTestTime] = useState(30);
+  const [timeLeft, setTimeLeft] = useState(testTime);
   const [isTyping, setIsTyping] = useState(false);
   const [testCompleted, setTestCompleted] = useState(false);
   const [mistakes, setMistakes] = useState(0);
   const [theme, setTheme] = useState(themes.default);
+  const [bestWpm, setBestWpm] = useState(0);
+  const [caretPosition, setCaretPosition] = useState({ top: 0, left: 0 });
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -22,6 +26,9 @@ const TypingBox = () => {
   const currentWordIndex =
     words.substring(0, currentIndex).split(" ").length - 1;
 
+  // CHAR Referecnce
+  const charRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
   // STATS
   const correctChars = input
     .split("")
@@ -29,7 +36,8 @@ const TypingBox = () => {
 
   const wordsTyped = correctChars / 5;
 
-  const wpm = Math.round(wordsTyped * (60 / (30 - timeLeft || 1)));
+  const timeSpent = testTime - timeLeft || 1;
+  const wpm = Math.round(wordsTyped * (60 / timeSpent));
 
   const accuracy =
     input.length === 0 ? 100 : Math.round((correctChars / input.length) * 100);
@@ -43,7 +51,7 @@ const TypingBox = () => {
   const restartTest = () => {
     setInput("");
     setIsTyping(false);
-    setTimeLeft(30);
+    setTimeLeft(testTime);
     setTestCompleted(false);
     setMistakes(0);
 
@@ -109,10 +117,42 @@ const TypingBox = () => {
     };
   }, [timeLeft]);
 
+  //   load saved WPN
+  useEffect(() => {
+    const savedBest = localStorage.getItem("best-wpm");
+    if (savedBest) {
+      setBestWpm(Number(savedBest));
+    }
+  }, []);
+
+  // save new best Score
+  useEffect(() => {
+    if (!testCompleted) return;
+
+    if (wpm > bestWpm) {
+      setBestWpm(wpm);
+      localStorage.setItem("best-wpm", String(wpm));
+    }
+  }, [testCompleted]);
+
+  //   TRACK ACTIVE CHARACTER POSITION
+  useEffect(() => {
+    const currentChar = charRefs.current[currentIndex];
+    if (currentChar) {
+      setCaretPosition({
+        top: currentChar.offsetTop,
+        left: currentChar.offsetLeft,
+      });
+    }
+  }, [currentIndex]);
+
   return (
-    <div
+    <motion.div
       className={`min-h-screen ${theme.background} ${theme.text} flex flex-col items-center justify-center px-6`}
       onClick={() => inputRef.current?.focus()}
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
     >
       {/* THEME SELECTOR */}
       <div className="flex gap-4 mb-10">
@@ -131,22 +171,74 @@ const TypingBox = () => {
           className="w-5 h-5 rounded-full bg-green-400"
         />
       </div>
+      <div className="flex gap-6 mb-10 font-mono text-sm">
+        {[15, 30, 60, 120].map((time) => (
+          <button
+            key={time}
+            onClick={() => {
+              if (isTyping) return;
+
+              setTestTime(time);
+              setTimeLeft(time);
+              restartTest();
+            }}
+            className={` cursor-pointer
+                text-lg
+        transition-colors
+        duration-200
+        ${testTime === time ? theme.accent : "text-gray-600"}`}
+          >
+            {time}{" "}
+          </button>
+        ))}
+      </div>
 
       {/* TIMER */}
       <div className="w-full max-w-5xl mb-12">
-        <div className="text-yellow-400 text-3xl font-mono font-bold">
-          {timeLeft}
+        <div className="flex items-center gap-6">
+          <div className={`text-3xl font-mono font-bold ${theme.accent}`}>
+            {timeLeft}
+          </div>
+
+          <div className="text-gray-600 font-mono text-lg">best: {bestWpm}</div>
         </div>
       </div>
 
       {/* TYPING AREA */}
       <div className="w-full max-w-5xl">
-        <div className="text-4xl font-mono leading-relaxed break-words select-none">
+        <div className="relative text-4xl font-mono leading-relaxed break-words select-none">
+          {!testCompleted && (
+            <span
+              className={`
+      absolute
+      w-[3px]
+      h-[55px]
+      rounded-full
+      animate-caret
+      transition-all
+      duration-75
+      shadow-[0_0_8px]
+      z-10
+      ${theme.caret}
+    `}
+              style={{
+                top: caretPosition.top,
+                left: caretPosition.left,
+              }}
+            />
+          )}
+
           {words.split("").map((char, index) => {
             let color = theme.sub;
 
             if (index < input.length) {
-              color = input[index] === char ? theme.text : "text-red-500";
+              const typedChar = input[index];
+
+              if (char === " " && typedChar === " ") {
+                color = theme.sub;
+              } else {
+                color = typedChar === char ? theme.text : "text-red-500";
+              }
             }
 
             const wordIndex = words.substring(0, index).split(" ").length - 1;
@@ -157,6 +249,9 @@ const TypingBox = () => {
             // DYNAMIC CARET
             return (
               <span
+                ref={(el) => {
+                  charRefs.current[index] = el;
+                }}
                 key={index}
                 className={`
   relative
@@ -166,24 +261,6 @@ const TypingBox = () => {
   ${isCurrentWord ? `${theme.active} rounded` : ""}
 `}
               >
-                {index === currentIndex && !testCompleted && (
-                  <span
-                    className={`
-    absolute
-    left-0
-    top-1
-    w-[3px]
-    h-[55px]
-    rounded-full
-    animate-caret
-    transition-all
-    duration-75
-    shadow-[0_0_8px]
-    ${theme.caret}
-  `}
-                  />
-                )}
-
                 {char}
               </span>
             );
@@ -192,40 +269,57 @@ const TypingBox = () => {
       </div>
 
       {/* RESULTS */}
-      {timeLeft === 0 && testCompleted && (
-        <div className="w-full max-w-5xl mt-20 flex gap-20 font-mono">
+      {/* RESULTS */}
+      {testCompleted && (
+        <div
+          className={`
+      w-full
+      max-w-5xl
+      mt-24
+      p-10
+      rounded-2xl
+      bg-zinc-950/40
+      backdrop-blur
+      flex
+      gap-20
+      font-mono
+    `}
+        >
           <div>
-            <p className="text-gray-500 text-sm mb-2 uppercase tracking-widest">
+            <p className="text-gray-500 text-sm mb-3 uppercase tracking-widest">
               WPM
             </p>
 
-            <h2 className="text-yellow-400 text-6xl font-bold">{wpm}</h2>
+            <h2 className={`text-7xl font-bold ${theme.accent}`}>{wpm}</h2>
           </div>
 
           <div>
-            <p className="text-gray-500 text-sm mb-2 uppercase tracking-widest">
+            <p className="text-gray-500 text-sm mb-3 uppercase tracking-widest">
               Accuracy
             </p>
 
-            <h2 className="text-yellow-400 text-6xl font-bold">{accuracy}%</h2>
+            <h2 className={`text-7xl font-bold ${theme.accent}`}>
+              {accuracy}%
+            </h2>
           </div>
+
           <div>
-            <p className="text-gray-500 text-sm mb-2 uppercase tracking-widest">
+            <p className="text-gray-500 text-sm mb-3 uppercase tracking-widest">
               Mistakes
             </p>
 
-            <h2 className="text-yellow-400 text-6xl font-bold">{mistakes}</h2>
-          </div>
-          <div>
-            <p className="text-gray-500 text-sm mb-2 uppercase tracking-widest">
-              Characters
-            </p>
-
-            <h2 className="text-yellow-400 text-6xl font-bold">
-              {charactersTyped}
-            </h2>
+            <h2 className={`text-7xl font-bold ${theme.accent}`}>{mistakes}</h2>
           </div>
         </div>
+      )}
+      {testCompleted && (
+        <p className="mt-8 text-gray-500 font-mono">
+          {wpm > 80
+            ? "insane speed ⚡"
+            : wpm > 40
+              ? "great typing 🔥"
+              : "keep practicing 🚀"}
+        </p>
       )}
 
       {/* RESTART MESSAGE */}
@@ -253,7 +347,7 @@ const TypingBox = () => {
 
           if (value[lastCharIndex] !== words[lastCharIndex])
             setMistakes((prev) => prev + 1);
-          if (value.endsWith(" ")) return;
+          //   if (value.endsWith(" ")) return;
           setInput(value);
 
           if (!isTyping) {
@@ -261,7 +355,7 @@ const TypingBox = () => {
           }
         }}
       />
-    </div>
+    </motion.div>
   );
 };
 
